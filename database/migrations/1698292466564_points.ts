e1fcff9c7bc3311e56786695f631219b67eff7bd
Liceth02usma <juanitausma02@gmail.com>
2023-10-26 18:36:04 -0500

    migration SQL

--- database/migrations/1698292466564_points.ts
+++ database/migrations/1698292466564_points.ts
@@ -6,10 +6,12 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('name').notNullable()
+      table.float('latitude').notNullable()
+      table.float('longitude').notNullable()
+      table.integer('route_id').unsigned()
+                                 .references('routes.id')
+                                 .onDelete('CASCADE')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
--- database/migrations/1698292606195_ratings.ts
+++ database/migrations/1698292606195_ratings.ts
@@ -3,19 +3,21 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'ratings'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.integer('stars')
+      table.string('comment')
+      table.dateTime('dateTime')
+      table.integer('trip_id').unsigned()
+                              .references('trips.id')
+                              .onDelete('CASCADE')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
