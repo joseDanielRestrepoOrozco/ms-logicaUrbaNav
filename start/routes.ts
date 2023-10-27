ea00b4a0a59fdddfd32de75c8293fea3dde94b82
Liceth02usma <juanitausma02@gmail.com>
2023-10-26 22:51:26 -0500

    Create Controllers

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,26 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+
+Route.post("/bill","BillsController.store");
+Route.get("/bill","BillsController.index");
+Route.get("/bill/:id","BillsController.show");
+Route.put("/bill/:id","BillsController.update");
+Route.delete("/bill/:id","BillsController.destroy");
+
+
+
+Route.post("/point","PointsController.store");
+Route.get("/point","PointsController.index");
+Route.get("/point/:id","PointsController.show");
+Route.put("/point/:id","PointsController.update");
+Route.delete("/point/:id","PointsController.destroy");
+
+
+
+Route.post("/rating","RatingsController.store");
+Route.get("/rating","RatingsController.index");
+Route.get("/rating/:id","RatingsController.show");
+Route.put("/rating/:id","RatingsController.update");
+Route.delete("/rating/:id","RatingsController.destroy");
