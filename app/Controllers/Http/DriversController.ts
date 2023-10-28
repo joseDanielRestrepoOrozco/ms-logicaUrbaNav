efe230bb6326474da5fd69e7c6bdd3bc3418c97a
joseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2023-10-27 21:49:16 -0500

    actualizando controlador de viajes

--- app/Controllers/Http/DriversController.ts
+++ app/Controllers/Http/DriversController.ts
@@ -2,46 +2,46 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Driver from 'App/Models/Driver'
 
 export default class DriversController {
-        //Create
-        public async store({ request }: HttpContextContract) {
-            let body = request.body()
-            const theDriver = await Driver.create(body)
-            return theDriver
-        }
-    
-        //Get
-        public async index({ request }: HttpContextContract) {
-            const page = request.input('page', 1)
-            const perPage = request.input('per_page', 20)
-            let drivers: Driver[] = await Driver.query().paginate(page, perPage)
-            return drivers
-        }
-    
-        /**
-         * esto puede ayudar cuando se haga la relacion entre conductor y vehiculo
-         * @returns 
-         */
-        // public async show({ params }: HttpContextContract) {
-        //     let theDriver: Driver = await Driver.query().where("id", params.id).preload('vehicle').firstOrFail()
-        //     return theDriver
-        // }
+    //Create
+    public async store({ request }: HttpContextContract) {
+        let body = request.body()
+        const theDriver = await Driver.create(body)
+        return theDriver
+    }
 
-        public async show({ params }: HttpContextContract) {
-            return Driver.findOrFail(params.id)
-        }
-    
-        //Update 
-        public async update({ params, request }: HttpContextContract) {
-            const body = request.body()
-            const theDriver: Driver = await Driver.findOrFail(params.id)
-            theDriver.isAvailable = body.isAvailable
-            return theDriver.save()
-        }
-    
-        //Delete
-        public async destroy({ params, response }: HttpContextContract) {
-            const theDriver: Driver = await Driver.findOrFail(params.id)
-            response.status(204)
-            return theDriver.delete()
-        }
+    //Get
+    public async index({ request }: HttpContextContract) {
+        const page = request.input('page', 1)
+        const perPage = request.input('per_page', 20)
+        let drivers: Driver[] = await Driver.query().paginate(page, perPage)
+        return drivers
+    }
+
+    /**
+     * esto puede ayudar cuando se haga la relacion entre conductor y vehiculo
+     * @returns 
+     */
+    // public async show({ params }: HttpContextContract) {
+    //     let theDriver: Driver = await Driver.query().where("id", params.id).preload('vehicle').firstOrFail()
+    //     return theDriver
+    // }
+
+    public async show({ params }: HttpContextContract) {
+        return Driver.findOrFail(params.id)
+    }
+
+    //Update 
+    public async update({ params, request }: HttpContextContract) {
+        const body = request.body()
+        const theDriver: Driver = await Driver.findOrFail(params.id)
+        theDriver.isAvailable = body.isAvailable
+        return theDriver.save()
+    }
+
+    //Delete
+    public async destroy({ params, response }: HttpContextContract) {
+        const theDriver: Driver = await Driver.findOrFail(params.id)
+        response.status(204)
+        return theDriver.delete()
+    }
 }
--- app/Controllers/Http/TripsController.ts
+++ app/Controllers/Http/TripsController.ts
@@ -2,14 +2,22 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Trip from 'App/Models/Trip'
 
 export default class TripsController {
-    //Create
+    /**
+     * Creacion de un viaje
+     * @param {HttpContextContract} request - solicitud
+     * @returns el viaje creado
+     */
     public async store({ request }: HttpContextContract) {
         let body = request.body()
         const theTrip = await Trip.create(body)
         return theTrip
     }
 
-    //Get
+    /**
+     * busqueda de todos los viaje en la base de datos
+     * @param {HttpContextContract} request - solicitud
+     * @returns los viajes con paginacion
+     */
     public async index({ request }: HttpContextContract) {
         const page = request.input('page', 1)
         const perPage = request.input('per_page', 20)
@@ -17,7 +25,39 @@ export default class TripsController {
         return trips
     }
 
+    /**
+     * mostrar un viaje dando un id como parametro
+     * @param {HttpContextContract} params - parametros dados por URL
+     * @returns el viaje si lo encontro
+     */
     public async show({ params }: HttpContextContract) {
         return Trip.findOrFail(params.id)
+    }
+
+    /**
+     * Actualiza los datos del viaje
+     * @param {HttpContextContract} request - solicitud con el body con informacion de los cambios
+     * @param {HttpContextContract} params - parametros dados por URL
+     * @returns hace efectivo el cambio en la base de datos
+     */
+    public async update({ params, request }: HttpContextContract) {
+        const body = request.body()
+        const theTrip: Trip = await Trip.findOrFail(params.id)
+        theTrip.date = body.date
+        theTrip.price = body.price
+        theTrip.status = body.status
+        return theTrip.save()
+    }
+
+    /**
+     * busca y elimina un viaje dando el id como parametro
+     * @param {HttpContextContract} response - respuesta por parte del servidor
+     * @param {HttpContextContract} params - parametros dados por URL
+     * @returns hace efectiva la eliminacion en la base de datos
+     */
+    public async destroy({ params, response }: HttpContextContract) {
+        const theTrip: Trip = await Trip.findOrFail(params.id)
+        response.status(204)
+        return theTrip.delete()
     }
 }
